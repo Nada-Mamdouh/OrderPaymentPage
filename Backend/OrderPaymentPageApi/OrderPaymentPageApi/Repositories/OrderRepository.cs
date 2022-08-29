e66629b5435bc98e4dbd2980a3d045e2203cdfc1
Nada-Mamdouh <85319019+Nada-Mamdouh@users.noreply.github.com>
2022-08-29 07:00:08 +0200

    Updating Deducting Logic inside deduct method
    Better logic for solving a bug in calculating the deducted money from each order

--- Backend/OrderPaymentPageApi/OrderPaymentPageApi/Repositories/OrderRepository.cs
+++ Backend/OrderPaymentPageApi/OrderPaymentPageApi/Repositories/OrderRepository.cs
@@ -38,58 +38,29 @@ namespace OrderPaymentPageApi.Repositories
 
         public void deduct(double amount, List<Order> orderedOrdersByDate)
         {
-            double orderPaidAmount = 0.0;
-            foreach(var order in orderedOrdersByDate)
+            double declinedAmount = amount;
+            if (amount <= 0) return;
+            else
             {
-                /** check if this row contains totally paid order */
-                if (order.PaidAmount == order.Total) continue;
-
-                /** check if this amount sent from user is equal to zero or a negative value
-                 * in other iterations this variable will be decreased in each iteration 
-                 * by the paid amount */
-                else if (amount <= 0) break;
-
-                /** if Total price of order is less than or equal to the amount sent 
-                 * then we will store the difference between amount and orderPaidAmount in db */
-                else if (order.Total <= amount)
+                List<Order> unpaidAndPartiallyPaidOrders = ordersOrderedByDate.Where(or => or.Total != or.PaidAmount).ToList();
+                foreach (var order in unpaidAndPartiallyPaidOrders)
                 {
-                    orderPaidAmount = amount - orderPaidAmount;
-                }
-                else 
-                {
-                /** else we will subtract the whole amount sent from user */
-                    orderPaidAmount = amount;
-                }
-                /** making sure we won't subtract/deduct any money greater than total amount */
-                orderPaidAmount = orderPaidAmount <= order.Total ? orderPaidAmount : order.Total;
-
-                /** decreasing Total value by the paid amount */
-                order.Total -= orderPaidAmount;
-
-                /** decreasing amount sent from user by the paid (subtracted) amount in each row */
-                amount -= orderPaidAmount;
-                Order updatedorder = dbcontext.Orders.FirstOrDefault(or => or.Id == order.Id);
-                /** making sure that paid amount from each order won't be greater than the total */
-                if (updatedorder != null)
-                {
-                    try
+                    double orderPaidAmount = 0.0;
+                    double totallyPaidMoneyFromOrderSoFar = order.Total - order.PaidAmount;
+                    if(declinedAmount < totallyPaidMoneyFromOrderSoFar)
                     {
-                        updatedorder.Id = order.Id;
-                        updatedorder.Title = order.Title;
-                        updatedorder.ItemPrice = order.ItemPrice;
-                        updatedorder.Quantity = order.Quantity;
-                        updatedorder.Total = order.Total;
-                        updatedorder.PaidAmount += orderPaidAmount;
-                        dbcontext.SaveChanges();
+                        orderPaidAmount = declinedAmount;
                     }
-                    catch (Exception ex)
+                    else
                     {
-                        // Console.WriteLine(ex.Message);
-                        throw (ex);
+                        orderPaidAmount = totallyPaidMoneyFromOrderSoFar;
                     }
-                }
-                
+                    declinedAmount -= orderPaidAmount;
 
+                    /** Updating Paid Amount in each row and save the updated value to database */
+                    order.PaidAmount += orderPaidAmount;
+                    dbcontext.SaveChanges();
+                }
             }
         }
 
